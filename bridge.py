f90da6897da3c48765177c57f7b3f6fe8b34b8a3
Salman <coldhills786@gmail.com>
2022-06-01 23:52:12 +0500

    bridge-contract

---
+++ bridge.py
@@ -0,0 +1,141 @@
+from pyteal import *
+
+def approval_program():
+    # Global States
+    bridge_fee = Bytes("bridge_fee")
+    token = Bytes("token")
+    amount = Bytes("amount")
+    target_network = Bytes("target_network")
+    target_token = Bytes("target_token")
+    target_address = Bytes("target_address")
+
+
+# Functions
+    @Subroutine(TealType.none)
+    def execute_asset_transfer(token: TealType.uint64, token_amount: TealType.uint64, token_receiver: TealType.bytes) -> Expr:
+        return Seq([
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.AssetTransfer,
+                TxnField.xfer_asset: token,
+                TxnField.asset_amount: token_amount,
+                TxnField.asset_receiver: token_receiver,
+                TxnField.fee: Int(0),
+                }),
+            InnerTxnBuilder.Submit()
+        ])
+
+    @Subroutine(TealType.none)
+    def is_asset_transfered(asset_id: TealType.uint64, amount: TealType.uint64) -> Expr:
+        return Assert(
+            And(
+                Global.group_size() == Int(2),
+                Gtxn[0].type_enum() == TxnType.AssetTransfer,
+                Gtxn[0].xfer_asset() == asset_id,
+                Gtxn[0].sender() == Txn.sender(),
+                Gtxn[0].asset_receiver() == Global.current_application_address(),
+                Gtxn[0].asset_amount() == amount,
+                )
+        )
+
+
+    @Subroutine(TealType.none)
+    def swap_token(token: TealType.uint64, _amount: TealType.uint64, _target_token: TealType.bytes, _target_network: TealType.uint64, _target_address: TealType.bytes):
+        return Seq([
+            # Exception ("BP: bad amount")
+            Assert(_amount > Int(0)),
+            # Exception ("BP: targetNetwork is requried")
+            Assert(_target_network > Int(0)),
+            App.globalPut(amount, _amount),
+            App.globalPut(target_token, _target_token),
+            App.globalPut(target_network, _target_network),
+            App.globalPut(target_address, _target_address),
+
+            # amount = SafeAmount.safeTransferFrom(token, from, address(this), amount);
+
+            # Verify that swapper has transfered assets to the application
+            is_asset_transfered(App.globalGet(token), _amount),
+        ])
+
+    @Subroutine(TealType.none)
+    def withdraw_token(token: TealType.uint64, receiver: TealType.bytes, amount: TealType.uint64):
+        # Scratch Variables
+        fee = ScratchVar(TealType.uint64)
+        token_amount = ScratchVar(TealType.uint64)
+        return Seq([
+            # Exception ("BP: bad amount")
+            Assert(amount > Int(0)),
+
+            token_amount.store(amount),
+            fee.store(App.globalGet(bridge_fee) * (amount / Int(10000))),
+
+
+            token_amount.store(token_amount.load() - fee.load()),
+
+            # IERC20(token).safeTransfer(payee, amount);
+
+            execute_asset_transfer(token, token_amount.load(), receiver)
+        ])
+
+    # CONSTRUCTOR
+    _bridge_fee = Btoi(Txn.application_args[0])
+    _token = Btoi(Txn.application_args[1])
+
+    on_creation = Seq(
+        App.globalPut(bridge_fee, _bridge_fee),
+        App.globalPut(token, _token),
+        Approve(),
+    )
+
+    _token = Btoi(Txn.application_args[1])
+    _amount = Btoi(Txn.application_args[2])
+    _target_network = Btoi(Txn.application_args[3])
+    _target_token = Txn.application_args[4]
+    _target_address = Txn.application_args[5]
+
+    # Method SWAP
+    on_swap = Seq([
+        # OPT-IN to Token from Application. 
+        execute_asset_transfer(App.globalGet(token), Int(0), Global.current_application_address()),
+        # SWAP TOKEN
+        swap_token(_token, _amount, _target_token, _target_network, _target_address),
+        Approve(),
+    ])
+
+    _token = Btoi(Txn.application_args[1])
+    _receiver = Btoi(Txn.application_args[2])
+    _amount = Btoi(Txn.application_args[3])
+
+    # Method WITHDRAW
+    on_withdraw = Seq([
+        withdraw_token(_token, _receiver, _amount),
+        Approve(),
+    ])
+
+    on_call_method = Txn.application_args[0]
+    on_call = Cond(
+        [on_call_method == Bytes("swap"), on_swap],
+        [on_call_method == Bytes("withdraw"), on_withdraw]
+    )
+    on_delete = Seq([ 
+        Reject(),
+    ])
+    on_update = Seq([ 
+        Reject(),
+    ])
+
+    program = Cond(
+        # Application Creation Call would be routed here.
+        [Txn.application_id() == Int(0), on_creation],
+
+         # All General Application calls will be routed here.
+        [Txn.on_completion() == OnComplete.NoOp, on_call],
+
+        # Reject DELETE and UPDATE Application Calls.
+        [Txn.on_completion() == OnComplete.UpdateApplication, on_update],
+        [Txn.on_completion() == OnComplete.DeleteApplication, on_delete]
+    )
+
+    return compileTeal(program, Mode.Application, version=5)
+
+print(approval_program())
---
+++ bridge.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+
+contract BridgePool {
+    // using SafeERC20 for IERC20;
+
+	uint256 constant FEE = 0.0025 * 10000; // 2.5%  fee
+
+    function swap(address token, uint256 amount, uint256 targetNetwork, address targetToken)
+    external returns(uint256) {
+        return _swap(msg.sender, token, amount, targetNetwork, targetToken, msg.sender);
+    }
+
+    function swapToAddress(address token,
+        uint256 amount,
+        uint256 targetNetwork,
+        address targetToken,
+        address targetAddress)
+    external returns(uint256) {
+        require(targetAddress != address(0), "BridgePool: targetAddress is required");
+        return _swap(msg.sender, token, amount, targetNetwork, targetToken, targetAddress);
+    }
+    // uint token = 8585858
+    // address targetToken = 0xJKLJFLZJFLfafkajl
+    // targetNetwork (BSC Chain ID)
+    // targetAddress (Wallet Address)
+    function _swap(address from, address token, uint256 amount, uint256 targetNetwork,
+        address targetToken, address targetAddress) internal returns(uint256) {
+		// require(from != address(0), "BP: bad from");
+		// require(token != address(0), "BP: bad token");
+		require(targetNetwork != 0, "BP: targetNetwork is requried");
+		// require(targetToken != address(0), "BP: bad target token");
+		require(amount != 0, "BP: bad amount");
+		// require(allowedTargets[token][targetNetwork] == targetToken, "BP: target not allowed");
+        amount = SafeAmount.safeTransferFrom(token, from, address(this), amount);
+        // emit BridgeSwap(from, token, targetNetwork, targetToken, targetAddress, amount);
+        return amount;
+    }
+
+    function withdrawToken(
+            address token,
+            address payee,  // receiver
+            uint256 amount
+            // bytes32 salt,
+            // bytes memory signature
+            )
+    external returns(uint256) {
+		require(token != address(0), "BP: bad token");
+		require(payee != address(0), "BP: bad payee");
+		// require(salt != 0, "BP: bad salt");
+		require(amount != 0, "BP: bad amount");
+        // bytes32 message = withdrawSignedMessage(token, payee, amount, salt);
+        // address _signer = signerUnique(message, signature);
+        // require(signers[_signer], "BridgePool: Invalid signer");
+
+        uint256 fee; // 0.25%
+        // address _feeDistributor = feeDistributor;
+        // if (_feeDistributor != address(0)) {
+
+            fee = amount * FEE / 10000;
+            amount = amount - fee;
+
+            // if (fee != 0) {
+            //     IERC20(token).safeTransfer(_feeDistributor, fee);
+            //     IGeneralTaxDistributor(_feeDistributor).distributeTax(token);
+            // }
+        // }
+        IERC20(token).safeTransfer(payee, amount);
+        // emit TransferBySignature(_signer, payee, token, amount, fee);
+        return amount;
+    }
+}
